e421ab7ad4c7f82f5cd36a4efef70071601e7f90
Krzysztof <krzys.zolnowski@gmail.com>
2023-09-07 12:13:42 +0200

    Module 3 task 4 done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,9 +1,11 @@
 package school.mjc.stage0.module3.task4;
 
+import static java.lang.Math.pow;
+
 public class FormulaCreator {
     public static void main(String[] args) {
-        char a = 97;
-        char b = 98;
-        System.out.println((9*(a*a) - 5*b + 2 + a - 7) * ((a+b - 4*(a*b))/2));
+        float a = 97;
+        float b = 98;
+        System.out.println((9*(pow(a, a)) - (5*b) + 2 + a - 7) * (((a+b) - 4*(a*b))/2));
     }
 }
